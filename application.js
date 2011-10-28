b82c890d8f8e1025a5f0b90326310daf168fc3bc
Linus Oleander <linus@oleander.nu>
2011-10-28 15:08:06 +0200

    Implementing #unorderedList and #orderedList

--- application.js
+++ application.js
@@ -3,12 +3,47 @@ App.templates = {};
 App.methods = {};
 
 /*
-  Converts Markdown lists into BBCode lists
+  Converts Markdown ordered lists into BBCode lists.
   @lines Array<String> A list of lines. Each line is in Markdown.
   @return Array<String> A list of lines. Each line is in BBCode.
 */
-App.methods.list = function(lines) {
-  var template = _.template($("#list-template").html());
+App.methods.orderedList = function(lines) {
+  var template = _.template($("#orderedList-template").html());
+  for (var i = 0; i < lines.length; i++) {
+    /* Is this a list item ?*/
+    if(lines[i].match(/^\s*\d+\. ([^\n]+)/)){
+      var matches = [lines[i].replace(/^\s*\d+\. /, "")];
+      lines[i] = null;
+      for (i = (i + 1); i < lines.length; i++) {
+        if(lines[i].match(/^\s*\d+\. ([^\n]+)/)){
+          matches.push(lines[i].replace(/^\s*\d+\. /, ""));
+          lines[i] = null;
+        } else {
+          break;
+        }
+      };
+      
+      /* 
+        This is the end of the list
+        Let's render it!
+      */
+      lines[i] = template({
+        list: matches.reverse()
+      });
+    }
+  };
+  
+  /* We've to remove all empty lines. */
+  return _.reject(lines, function(line){ return ! line; });  
+};
+
+/*
+  Converts Markdown unordered lists into BBCode lists.
+  @lines Array<String> A list of lines. Each line is in Markdown.
+  @return Array<String> A list of lines. Each line is in BBCode.
+*/
+App.methods.unorderedList = function(lines) {
+  var template = _.template($("#unorderedList-template").html());
   for (var i = 0; i < lines.length; i++) {
     /* Is this a list item ?*/
     if(lines[i].match(/^\s*- ([^\n]+)/)){
@@ -84,7 +119,10 @@ $(function() {
   
   /* Line specific methods */
   var lines = content.match(/\n?.*\n/g);
-  lines = App.methods.list(lines);
+  _.each(["unorderedList", "orderedList"], function(method) {
+    lines = App.methods[method](lines);
+  });
+  
   
   from.html(lines.join("\n"));
 });
