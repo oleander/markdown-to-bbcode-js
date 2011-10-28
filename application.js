94d83a6a7e268e818513cebf251140cdf06fefea
Linus Oleander <linus@oleander.nu>
2011-10-28 16:34:00 +0200

    Implementing #code

--- application.js
+++ application.js
@@ -11,18 +11,18 @@ App.methods.orderedList = function(lines) {
   var template = _.template($("#orderedList-template").html());
   for (var i = 0; i < lines.length; i++) {
     /* Is this a list item ?*/
-    if(lines[i].match(/^\s*\d+\. ([^\n]+)/)){
+    if (lines[i].match(/^\s*\d+\. ([^\n]+)/)) {
       var matches = [lines[i].replace(/^\s*\d+\. /, "")];
       lines[i] = null;
       for (i = (i + 1); i < lines.length; i++) {
-        if(lines[i].match(/^\s*\d+\. ([^\n]+)/)){
+        if (lines[i].match(/^\s*\d+\. ([^\n]+)/)) {
           matches.push(lines[i].replace(/^\s*\d+\. /, ""));
           lines[i] = null;
         } else {
           break;
         }
       };
-      
+
       /* 
         This is the end of the list
         Let's render it!
@@ -32,9 +32,11 @@ App.methods.orderedList = function(lines) {
       });
     }
   };
-  
+
   /* We've to remove all empty lines. */
-  return _.reject(lines, function(line){ return ! line; });  
+  return _.reject(lines, function(line) {
+    return !line;
+  });
 };
 
 /*
@@ -46,18 +48,18 @@ App.methods.unorderedList = function(lines) {
   var template = _.template($("#unorderedList-template").html());
   for (var i = 0; i < lines.length; i++) {
     /* Is this a list item ?*/
-    if(lines[i].match(/^\s*- ([^\n]+)/)){
+    if (lines[i].match(/^\s*- ([^\n]+)/)) {
       var matches = [lines[i].replace(/^\s*- /, "")];
       lines[i] = null;
       for (i = (i + 1); i < lines.length; i++) {
-        if(lines[i].match(/^\s*- ([^\n]+)/)){
+        if (lines[i].match(/^\s*- ([^\n]+)/)) {
           matches.push(lines[i].replace(/^\s*- /, ""));
           lines[i] = null;
         } else {
           break;
         }
       };
-      
+
       /* 
         This is the end of the list
         Let's render it!
@@ -67,9 +69,11 @@ App.methods.unorderedList = function(lines) {
       });
     }
   };
-  
+
   /* We've to remove all empty lines. */
-  return _.reject(lines, function(line){ return ! line; });  
+  return _.reject(lines, function(line) {
+    return !line;
+  });
 };
 
 /*
@@ -79,6 +83,42 @@ App.methods.unorderedList = function(lines) {
 */
 App.methods.url = function(content) {
   return content.replace(/\[([^\]]+)]\(([^)]+)\)/gmi, '[URL="$2"]$1[/URL]');
+};
+
+/*
+  Converts Markdown code tag into BBCode's code tag.
+  @content String The raw document. Each line is in Markdown.
+  @return String The raw document. Each line is in BBCode.
+  A Markdown code block looks like this;
+  ``` html
+  Some HTML code
+  ```
+  
+  ```
+  Some HTML code
+  ```
+*/
+App.methods.code = function(content) {
+  var regexp = /```\s*(([^\n]+))?([^```]+)```/img;
+  
+  var getType = function(match) {
+    var type = match.trim().toUpperCase();
+    if (_.include(["CODE", "HTML", "PHP"], type)) {
+      return type;
+    } else {
+      return "CODE";
+    }
+  };
+  
+  return content.replace(regexp, function(full, none, type, code) {
+    var type = getType(type);
+    if(code.trim().length === 0){
+      code = type;
+    } else {
+      code = code.trim();
+    }
+    return "[" + type + "]\n" + code + "\n[/" + type + "]";
+  });
 };
 
 /*
@@ -110,19 +150,22 @@ App.methods.underscore = function(content) {
 
 $(function() {
   var from = $("#from");
+  var to = $("#to");
+
   var content = from.html();
-  
+  var original = content;
+
   /* String specific methods */
-  _.each(["url", "strong", "italic", "underscore"], function(method) {
+  _.each(["url", "strong", "italic", "underscore", "code"], function(method) {
     content = App.methods[method](content);
   });
-  
+
   /* Line specific methods */
   var lines = content.match(/\n?.*\n/g);
   _.each(["unorderedList", "orderedList"], function(method) {
     lines = App.methods[method](lines);
   });
-  
-  
-  from.html(lines.join("\n"));
+
+  from.html(original.replace("\n", "<br/>"));
+  to.html(lines.join("<br/>").replace(/\n/, "<br/>"));
 });
